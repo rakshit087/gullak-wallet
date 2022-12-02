8fc7046db3df014aa6c6fbb07d20d38dc5ef06a5
Rakshit Arora <rakshit087@gmail.com>
2022-12-03 01:52:09 +0530

    feat(Global): migrated to vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+    react()
+  ],
+});
