ee216cf06cd7284a1193482b6987513d2d2e17c1
Rupali Shah <rupalishah040@gmail.com>
2022-12-03 03:59:54 +0530

    pwa push

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,24 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        name: 'Gullak Wallet',
+        short_name: 'GWallet',
+        description: 'Our Gullak wallet',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './src/assets/192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
     react()
   ],
