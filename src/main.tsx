5fd0c80bb15e63597a2427accba586d51a245f50
Rakshit Arora <rakshit087@gmail.com>
2022-12-03 02:24:40 +0530

    feat: added prettierc

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import { AuthPage } from './pages/AuthPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "/auth",
-    element: <AuthPage />
-  }
+    path: '/auth',
+    element: <AuthPage />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
