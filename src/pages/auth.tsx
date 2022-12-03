e62931d3a01b35617803d9c27368122025418078
Rakshit Arora <rakshit087@gmail.com>
2022-12-03 07:48:39 +0530

    finished auth page

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,4 +1,5 @@
 import { Button, Flex } from '@chakra-ui/react';
+import Image from 'next/image';
 import { FaGoogle } from 'react-icons/fa';
 import { loginWithGoogle, loginWithTwitter } from '../services/autentication';
 
@@ -8,15 +9,25 @@ export default function AuthPage() {
       h={'100vh'}
       bgColor={'#252528'}
       w={'full'}
+      flexDir={'column'}
       justifyContent={'center'}
       alignItems={'center'}
     >
+      <Image
+        src={'/assets/gullak-transparent.png'}
+        alt={'logo'}
+        width={512}
+        height={512}
+      />
       <Button
         colorScheme="gray"
         leftIcon={<FaGoogle />}
         onClick={() => {
           loginWithGoogle();
         }}
+        width={72}
+        borderRadius={32}
+        mb={4}
       >
         Continue with Google
       </Button>
@@ -26,9 +37,11 @@ export default function AuthPage() {
         onClick={() => {
           loginWithTwitter();
         }}
+        width={72}
+        borderRadius={32}
       >
         Continue with Twitter
       </Button>
     </Flex>
   );
-};
+}
