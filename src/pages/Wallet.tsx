4ecee9ad470aa41a0035b056a44cbff9286e9b93
Rupali Shah <rupalishah040@gmail.com>
2022-12-04 04:57:43 +0530

    merge conflit

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,12 +1,34 @@
 import { Badge, Box, Flex, IconButton, Button, Grid, GridItem, Heading } from '@chakra-ui/react';
-import { Card, CardHeader, CardBody, CardFooter, Text, Input } from '@chakra-ui/react';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  FormLabel,
+  FormControl,
+  ModalCloseButton,
+} from '@chakra-ui/react';
 import copy from 'copy-to-clipboard';
 import { IoMdWallet } from 'react-icons/io';
 import { FiCopy } from 'react-icons/fi';
 import Image from 'next/image';
 import { BiMessageRoundedDots } from 'react-icons/bi';
+import { useDisclosure } from '@chakra-ui/react';
 
 export default function WalletScreen() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+
   return (
     <Flex>
       <Grid
@@ -110,6 +132,23 @@ export default function WalletScreen() {
             </CardFooter>
           </Card>
         </GridItem>
+        <GridItem
+          rowSpan={4}
+          colSpan={5}
+        >
+          <Input
+            bg={'#333333'}
+            p={'17px'}
+            color={'white'}
+            variant="unstyled"
+            placeholder="Enter Amount"
+            overflow="hidden"
+            fontSize={'20px'}
+            ml={70}
+            w={'full'}
+            _placeholder={{ opacity: 1, color: 'gray.500' }}
+          />
+        </GridItem>
 
         <GridItem
           rowSpan={4}
@@ -123,29 +162,13 @@ export default function WalletScreen() {
             bg="#c9f99c"
             size="lg"
             w="100%"
-            h="10"
-            m={-10}
+            h="12"
+            ml={13}
           >
             Add Funds
           </Button>
         </GridItem>
-        <GridItem
-          rowSpan={2}
-          colSpan={5}
-          alignItems={'center'}
-          ml={'10'}
-          w="100%"
-          h="10"
-        >
-          <Button
-            bg="#c9f99c"
-            size="lg"
-            w="100%"
-            h="10"
-          >
-            Withdraw Funds
-          </Button>
-        </GridItem>
+
         <GridItem
           rowSpan={4}
           colSpan={5}
@@ -158,10 +181,47 @@ export default function WalletScreen() {
             bg="#c9f99c"
             size="lg"
             w="100%"
-            h="10"
+            h="12"
+            ml={13}
           >
             Send Funds
           </Button>
+          <Modal
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Create your account</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody pb={6}>
+                <FormControl>
+                  <FormLabel>First name</FormLabel>
+                  <Input
+                    ref={initialRef}
+                    placeholder="First name"
+                  />
+                </FormControl>
+
+                <FormControl mt={4}>
+                  <FormLabel>Last name</FormLabel>
+                  <Input placeholder="Last name" />
+                </FormControl>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button
+                  colorScheme="blue"
+                  mr={3}
+                >
+                  Save
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </GridItem>
       </Grid>
     </Flex>
