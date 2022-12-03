0ed4b72045ec8e1168c0f9fc22b0e886dd698eff
Rakshit Arora <rakshit087@gmail.com>
2022-12-03 19:20:21 +0530

    moved dashboard

--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -1,10 +1,14 @@
-import { Badge, Box, Flex, IconButton } from '@chakra-ui/react';
+import { Badge, Box, Flex, IconButton, Button, Grid, GridItem } from '@chakra-ui/react';
+import { Card, CardHeader, CardBody, CardFooter, Text } from '@chakra-ui/react';
 import copy from 'copy-to-clipboard';
+
 import { IoMdWallet } from 'react-icons/io';
 import { FiCopy } from 'react-icons/fi';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 export const DashboardScreen = () => {
   return (
+    <Flex direction={'column'}>
     <Flex
       h={'100vh'}
       bgColor={'#252528'}
@@ -26,7 +30,7 @@ export const DashboardScreen = () => {
         <IconButton
           pt={0}
           pl={300}
-          variant="outline"
+          variant="unStyle"
           fontSize="30px"
           h="50%"
           w="100%"
@@ -76,12 +80,10 @@ export const DashboardScreen = () => {
             0x124...36484
           </Badge>
           <IconButton
-            mt={-50}
+            mt={0}
             ml={-3}
-            variant="outline"
+            variant="unstyle"
             fontSize="10px"
-            h="50%"
-            w="100%"
             aria-label="Copy icon"
             onClick={() => {
               copy('Text');
@@ -91,5 +93,50 @@ export const DashboardScreen = () => {
         </Box>
       </Box>
     </Flex>
+    <Grid
+      h="200px"
+      templateColumns="repeat(5, 1fr)"
+      gap={4}
+      mt={-300}
+      ml={'5'}
+      mr={'5'}
+    >
+      <GridItem
+        rowSpan={4}
+        colSpan={5}
+      >
+        <Card
+          bg={'#333333'}
+          overflow="hidden"
+        >
+          <CardFooter
+            justify="space-between"
+            flexWrap="wrap"
+            flexDirection={'row'}
+            sx={{
+              '& > button': {
+                minW: '10px',
+              },
+            }}
+          >
+            <Text color={'white'}>Trip to Goa</Text>
+            <Button mt={-2}>$ 500</Button>
+          </CardFooter>
+        </Card>
+      </GridItem>
+    </Grid>
+
+    <IconButton
+      variant={'floating'}
+      pos="fixed"
+      w="50px"
+      h="50px"
+      bg={'#c9f99c'}
+      ml={'47vh'}
+      mt={'90vh'}
+      aria-label="add btn"
+      icon={<AiOutlinePlus />}
+    ></IconButton>
+  </Flex>
   );
 };
