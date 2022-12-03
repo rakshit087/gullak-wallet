34915e0ac974f0bf0b33e1480087b580dc7e0f01
Rupali Shah <rupalishah040@gmail.com>
2022-12-04 03:28:12 +0530

    wallet page edit

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,8 @@
 import { Badge, Box, Flex, IconButton, Button, Grid, GridItem, Heading } from '@chakra-ui/react';
 import { Card, CardHeader, CardBody, CardFooter, Text, Input } from '@chakra-ui/react';
+import copy from 'copy-to-clipboard';
+import { IoMdWallet } from 'react-icons/io';
+import { FiCopy } from 'react-icons/fi';
 import Image from 'next/image';
 import { BiMessageRoundedDots } from 'react-icons/bi';
 
@@ -39,7 +42,72 @@ export default function WalletScreen() {
                   minW: '10px',
                 },
               }}
-            ></CardFooter>
+            >
+              <IconButton
+                pt={0}
+                pl={300}
+                variant="unStyle"
+                fontSize="30px"
+                h="50%"
+                w="100%"
+                aria-label="Wallet icon"
+                icon={<IoMdWallet />}
+              ></IconButton>
+              <Box
+                w="100%"
+                pl={4}
+                fontSize="xl"
+                fontWeight="bold"
+                color="black"
+              >
+                <Badge
+                  bg="#c9f99c"
+                  fontSize="2em"
+                  color="black"
+                >
+                  $ 500
+                </Badge>
+              </Box>
+              <Box
+                w="100%"
+                pt={75}
+                pl={110}
+              >
+                <Badge
+                  bg="#c9f99c"
+                  fontSize="12px"
+                  fontWeight={0.8}
+                >
+                  Wallet Address
+                </Badge>
+              </Box>
+              <Box
+                w="100%"
+                pt={0}
+                mt="-1"
+                pl={118}
+              >
+                <Badge
+                  bg="#c9f99c"
+                  fontSize="12px"
+                  as={'p'}
+                  className="txt"
+                >
+                  0x124...36484
+                </Badge>
+                <IconButton
+                  mt={0}
+                  ml={-3}
+                  variant="unstyle"
+                  fontSize="10px"
+                  aria-label="Copy icon"
+                  onClick={() => {
+                    copy('Text');
+                  }}
+                  icon={<FiCopy />}
+                ></IconButton>
+              </Box>
+            </CardFooter>
           </Card>
         </GridItem>
 
