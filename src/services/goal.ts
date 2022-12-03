d31483c29e80d7dbd92cc7ca06cb94fc0358a31e
gaharavara <kumarsinghabhishek59@gmail.com>
2022-12-03 15:11:09 +0530

    Write method for creating goal.

--- src/services/goal.ts
+++ src/services/goal.ts
@@ -1,7 +1,20 @@
-export function createGoal(name: string, addresses: Array<string>): any {
+import {
+  createAndDeploySafe,
+  getContractNetworks,
+} from "./safe";
+
+export async function createGoal(name: string, addresses: Array<string>): any {
+  const invoker = null; // TODO: decide if the invoker is stored in window
+  const contractNetworks = await getContractNetworks(invoker);
+  const deployedSafe = await createAndDeploySafe(invoker,addresses,contractNetworks);
+  // TODO: see if id deployed safe returns something that can be taken as id
+  console.log("this is deployed safe: ", {deployedSafe});
+  const id = deployedSafe.id | 1;
   // name: addresses of created safe
   // save the userPrivate/public id: name: address
-  return {id: {name, addresses}};
+  const goal:any = {};
+  goal[id] = {name, addresses};
+  return goal;
 }
 
 export function listGoals() {
